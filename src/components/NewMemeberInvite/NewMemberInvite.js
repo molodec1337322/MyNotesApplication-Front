4eb62c4612e729b92b4f7dbb5318f5f9243a570f
molodec1337322 <sshibanov1@yandex.ru>
2023-08-03 23:54:35 +0300

    added fetching data from backend server

--- src/components/NewMemeberInvite/NewMemberInvite.js
+++ src/components/NewMemeberInvite/NewMemberInvite.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Form, FormControl, Row} from "react-bootstrap";
 
-function NewMemberInvite({setActive, email}){
+function NewMemberInvite({setActive, currentBoardId}){
 
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
+    function changeInputLogin(e){
+        e.persist()
+        setEmail(prev => {
+            return{
+                ...prev,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    async function sendInvitation(){
+
+    }
+
+    return(
+        <div className="form">
+            <h2>Вход</h2>
+            <Form onSubmit={sendInvitation}>
+                <Form.Group as={Row} className="mb-4" controlId="formPlaintextEmail">
+                    <FormControl name="email" type="memberEmail" placeholder="Почта (example@example.com)" value={email} onChange={changeInputLogin}/>
+                </Form.Group>
+
+                <div className="d-flex justify-content-center">
+                    <Button className="mx-lg-2 mx-0 my-lg-0 my-2" variant="success" type="submit">Отправить</Button>
+                </div>
+
+                <div  className="d-flex justify-content-center">
+                    <p id="showErrorMessage">{message}</p>
+                </div>
+            </Form>
+        </div>
+    )
 }
 
 export default NewMemberInvite;
