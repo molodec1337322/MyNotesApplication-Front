2d725f23e5ffcc03e8cd292da761fe83cd629e11
molodec1337322 <sshibanov1@yandex.ru>
2023-08-05 23:33:36 +0300

    some fixes

--- src/components/NoteCard/CreateNewNoteCardButton.js
+++ src/components/NoteCard/CreateNewNoteCardButton.js
@@ -14,7 +14,14 @@ function CreateNewNoteCardButton({onAddNoteHandler, columnId, boardId}){
 
     return(
         <div>
-            <Button variant="outline-success" className="rounded-circle btn-lg" onClick={CreateNewNote}>+</Button>
+
+            <Button variant="outline-success" className="rounded-circle btn-lg" onClick={CreateNewNote}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="46" fill="currentColor"
+                     className="bi bi-plus-lg" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                          d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                </svg>
+            </Button>
             <MyModal active={isModalActive} setActive={setModalActive}>
                 <CreateNewNoteWindow setActive={setModalActive} onAddNoteHandler={onAddNoteHandler} columnId={columnId} boardId={boardId}/>
             </MyModal>
