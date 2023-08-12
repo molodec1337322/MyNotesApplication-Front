b5c0e7db56bc54c9fe521d628f9075e8f89dee8c
molodec1337322 <sshibanov1@yandex.ru>
2023-08-12 14:39:49 +0300

    some visual fix

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -151,51 +151,83 @@ function Home() {
                        onBoardChanged={onBoardChangeHandler}
             />
 
-        board =
-            <ColumnsPlaceholder columns={columns} setColumns={setColumns} notes={notes}
-                                setNotes={setNotes} currentBoardId={currentBoardId}/>
+        if(currentBoardId === -1){
+            board =
+                <div>
+                    <Row>
+                        <Col md="6" className="d-flex justify-content-center">
+                            <div className="BoardsListMain">
+                                <h4>Ваши доски</h4>
 
-        boardNav =
-            <Row>
-                <Col>
-                    <h4>{currentBoardName}</h4>
-                </Col>
+                                <div>
+                                    {ownedBoards?.map((board, index) => (
+                                        <ul><Button variant="light" onClick={() => {onBoardChangeHandler(board.name, board.id, true)}}>{board.name}</Button></ul>
+                                    ))}
+                                </div>
+                            </div>
+                        </Col>
 
-                <Col md="1">
-                    <div className="d-flex flex-row">
-                        <DropdownButton variant="light" className="mx-lg-2 mx-0 my-lg-0 my-2" id="dropdown-basic-button" title={
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                 className="bi bi-people" viewBox="0 0 16 16">
-                                <path
-                                    d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
-                            </svg>}>
-                            <Dropdown.Item><h5>Создатели</h5></Dropdown.Item>
-                            <Dropdown.Divider />
-                            {ownersBoardUser?.map((owner, index) => (
-                                <Dropdown.Item>{owner.username}</Dropdown.Item>
-                            ))}
-                            <Dropdown.Divider />
-                            <Dropdown.Item><h5>Гости</h5></Dropdown.Item>
-                            <Dropdown.Divider />
-                            {guestBoardUsers?.map((guest, index) => (
-                                <Dropdown.Item>{guest.username}</Dropdown.Item>
-                            ))}
-                        </DropdownButton>
-                        <Button variant="light" className="mx-lg-2 mx-0 my-lg-0 my-2" disabled={auth.isBoardOwner? false : true} onClick={() => setInviteNewMemberModalActive(true)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                 className="bi bi-person-add" viewBox="0 0 16 16">
-                                <path
-                                    d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
-                                <path
-                                    d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
-                            </svg>
-                        </Button>
-                        <MyModal active={inviteNewMemberModalActive} setActive={setInviteNewMemberModalActive}>
-                            <NewMemberInvite setActive={setInviteNewMemberModalActive} boardId={currentBoardId}/>
-                        </MyModal>
-                    </div>
-                </Col>
-            </Row>
+                        <Col md="6" className="d-flex justify-content-center">
+                            <div className="BoardsListMain">
+                                <h4>Гостевые доски</h4>
+
+                                <div>
+                                    {guestBoards?.map((board, index) => (
+                                        <Button variant="light" onClick={() => {onBoardChangeHandler(board.name, board.id, false)}}>{board.name}</Button>
+                                    ))}
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+        }
+        else{
+            board =
+                <ColumnsPlaceholder columns={columns} setColumns={setColumns} notes={notes}
+                                    setNotes={setNotes} currentBoardId={currentBoardId}/>
+
+            boardNav =
+                <Row>
+                    <Col>
+                        <h4>{currentBoardName}</h4>
+                    </Col>
+
+                    <Col md="1">
+                        <div className="d-flex flex-row">
+                            <DropdownButton variant="light" className="mx-lg-2 mx-0 my-lg-0 my-2" id="dropdown-basic-button" title={
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                     className="bi bi-people" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
+                                </svg>}>
+                                <Dropdown.Item><h5>Создатели</h5></Dropdown.Item>
+                                <Dropdown.Divider />
+                                {ownersBoardUser?.map((owner, index) => (
+                                    <Dropdown.Item>{owner.username}</Dropdown.Item>
+                                ))}
+                                <Dropdown.Divider />
+                                <Dropdown.Item><h5>Гости</h5></Dropdown.Item>
+                                <Dropdown.Divider />
+                                {guestBoardUsers?.map((guest, index) => (
+                                    <Dropdown.Item>{guest.username}</Dropdown.Item>
+                                ))}
+                            </DropdownButton>
+                            <Button variant="light" className="mx-lg-2 mx-0 my-lg-0 my-2" disabled={auth.isBoardOwner? false : true} onClick={() => setInviteNewMemberModalActive(true)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                     className="bi bi-person-add" viewBox="0 0 16 16">
+                                    <path
+                                        d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
+                                    <path
+                                        d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
+                                </svg>
+                            </Button>
+                            <MyModal active={inviteNewMemberModalActive} setActive={setInviteNewMemberModalActive}>
+                                <NewMemberInvite setActive={setInviteNewMemberModalActive} boardId={currentBoardId}/>
+                            </MyModal>
+                        </div>
+                    </Col>
+                </Row>
+        }
     }
     else{
         buttons =
